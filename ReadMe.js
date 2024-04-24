7043e63dc9c4a57e1481f5f264b5a31c2eb62a23
Yisaterese <151354744+Yisaterese@users.noreply.github.com>
2024-04-24 23:10:17 +0100

    added code

--- ReadMe.js
+++ ReadMe.js
@@ -16,11 +16,29 @@ function myClasses(classes){
 }
 
 function expenses(object){
+  let total = 0
   for (const key in object) {
-    if (Object.hasOwnProperty.call(object, key)) {
-      const element = object[key];
-      
-    }
+    total += object[key].amount
   }
+  return total
 }
-module.exports = {classTestScores,addScores,squareScores,myClasses}
+let book1 ={author:"author",title:"title"}
+let book2 ={author1:"author1",title1:"title1"}
+let book3 = {author2:"author2",title2:"title2"}
+let Emily ={name:"Emily"}
+let Jack ={name:"jack"}
+let Sophia ={name:"Sophia"}
+
+let books = [book1,book2,book3]
+let members = [Emily,Jack,Sophia]
+
+function bookDistribution(books,members){
+  for (const iterator of books) {
+    for (const key of members) {
+      members[key] = books[iterator] 
+      }
+  }
+  return members
+}
+console.log(bookDistribution(books,members))
+module.exports = {classTestScores,addScores,squareScores,myClasses,expenses,bookDistribution}
--- readMe.test.js
+++ readMe.test.js
@@ -1,4 +1,6 @@
-let {classTestScores, addScores,squareScores,myClasses} = require("./ReadMe")
+let {classTestScores, addScores,squareScores,myClasses,expenses,bookDistribution} = require("./ReadMe")
+
+
 
 test("score equals 70 or above ",()=>{
   expect(classTestScores([56,34,78,98,67,45,70,80,74,45])).toEqual([78, 98,70, 80, 74])
@@ -15,3 +17,34 @@ test("square every score in an array ",()=>{
 test("test for afternoon classes",()=>{
   expect(myClasses(["9:00 AM", "11:00 AM","1:00 PM","3:00 PM","5:00 PM"])).toEqual(["1:00 PM","3:00 PM","5:00 PM"])
 })
+
+test("test total ammount spent",()=>{
+  let groceries ={categorie:"groceries",amount: 150}
+  let dining = {categorie:"dining out",amount: 100}
+  let transportation = {categorie:"transportation",amount: 50}
+  let entertainment ={categorie:"entertainment", amount: 80 }
+ 
+  var objects = {groceries,dining,transportation,entertainment}
+  expect(expenses(objects)).toBe(380)
+})
+
+test("assign book to each member"){
+ let book1 ={author:"author",title:"title"}
+ let book2 ={author1:"author1",title1:"title1"}
+ let book3 = {author2:"author2",title2:"title2"}
+ let Emily ={name:"Emily"}
+let Jack ={name:"jack"}
+let Sophia ={name:"Sophia"}
+
+let books = [book1,book2,book3]
+let members = [Emily,Jack,Sophia]
+
+// let newMembers[
+//   Emily ={name:"Emily",book1},
+//   Jack ={name:"jack",book2},
+//   Sophia ={name:"Sophia",book3}
+// ]
+
+expect(bookDistribution(books,members)).toEqual(newMembers)
+
+}
